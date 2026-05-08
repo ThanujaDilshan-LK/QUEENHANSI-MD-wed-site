45c8f65bbc53023d23b648db3e8fb22d8ac16747
ThanujaDilshan-LK <thanujadilsahh2011@gmail.com>
2026-05-08 16:26:01 +0530

    Initialize package.json for queen-hansi-site

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "queen-hansi-site",
+  "version": "1.0.0",
+  "description": "Thanuja Dilshan Web",
+  "main": "index.html",
+  "scripts": {
+    "start": "echo 'Serving static site' && exit 0"
+  },
+  "author": "THANUJADILSHAN™",
+  "license": "MIT"
+}
